b34eabda6a610043a30ff46f09b8a0a2bc0eb2ea
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-01-13 14:05:22 +0600

    chapter based

--- assets/lodash.d3a00440.js
+++ assets/lodash.d3a00440.js
@@ -1,4 +1,4 @@
-import{N as jt}from"./index.20d3d822.js";var Ri={exports:{}};/**
+import{N as jt}from"./index.f0bb7909.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
